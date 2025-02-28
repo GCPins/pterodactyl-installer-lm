b074bac26f1ac0fa463596d8c6463cbaf4b63509
GC; Pins <64855897+GCPins@users.noreply.github.com>
2025-02-27 19:03:33 -0500

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -52,7 +52,7 @@ export SUPPORTED=false
 export PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz"
 export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
 export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
-export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"}
+export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/GCPins/pterodactyl-installer-lm"}
 export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
 
 # Colors
@@ -125,7 +125,7 @@ welcome() {
   output "Pterodactyl panel installation script @ $SCRIPT_RELEASE"
   output ""
   output "Copyright (C) 2018 - 2025, Vilhelm Prytz, <vilhelm@prytznet.se>"
-  output "https://github.com/pterodactyl-installer/pterodactyl-installer"
+  output "https://github.com/GCPins/pterodactyl-installer-lm"
   output ""
   output "This script is not associated with the official Pterodactyl Project."
   output ""
@@ -548,9 +548,9 @@ rocky | almalinux)
   ;;
 esac
 
-# exit if not supported
-if [ "$SUPPORTED" == false ]; then
-  output "$OS $OS_VER is not supported"
-  error "Unsupported OS"
-  exit 1
-fi
+## exit if not supported
+#if [ "$SUPPORTED" == false ]; then
+#  output "$OS $OS_VER is not supported"
+#  error "Unsupported OS"
+#  exit 1
+#fi
